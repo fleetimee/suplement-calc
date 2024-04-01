2f16bad61b08e9419a276c8463b08126a967dfb0
Novian Andika <novian.andika@students.amikom.ac.id>
2024-04-01 13:58:51 +0700

    Update Floating Action Button styling

--- src/components/floating-action-button.tsx
+++ src/components/floating-action-button.tsx
@@ -3,8 +3,8 @@ import { Button } from "./ui/button";
 
 export function FloatingActionButton() {
   return (
-    <Button className="fixed bottom-4 right-4 p-4 bg-primary  rounded-full shadow-lg text-white">
-      <Plus className="h-2 w-2" />
+    <Button className="fixed bottom-4 right-4 p-8 bg-primary rounded-full shadow-lg text-white flex items-center justify-center">
+      <Plus className="h-6 w-6" />
     </Button>
   );
 }
